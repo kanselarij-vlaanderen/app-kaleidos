368b898a9d44303fd646c99b694e4c49631a2ba7
Joachim Zeelmaekers <joachimzeelmaekers@macbook-pro-4.iconos.be>
2019-01-14 15:11:08 +0100

    changed the docker compose to be able to add a new micro service

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -44,6 +44,10 @@ defmodule Dispatcher do
     Proxy.forward conn, path, "http://resource/comments/"
   end
 
+  match "/*path" do
+    Proxy.forward conn, path, "http://session-number-service/"
+  end
+
   match _ do
     send_resp( conn, 404, "Route not found.  See config/dispatcher.ex" )
   end
