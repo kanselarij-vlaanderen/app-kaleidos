51e6c7e056473eedd58b13b2ecca250b3e0e1102
Joachim Zeelmaekers <joachimzeelmaekers@MacBook-Pro-4.local>
2019-01-20 17:21:46 +0100

    added a new path in the dispatcher to call the session-number-service

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -52,6 +52,10 @@ defmodule Dispatcher do
     Proxy.forward conn, path, "http://resource/capacities/"
   end
 
+  match "/session-service/*path" do
+    Proxy.forward conn, path, "http://session-number-service/"
+  end
+
   match _ do
     send_resp( conn, 404, "Route not found.  See config/dispatcher.ex" )
   end
