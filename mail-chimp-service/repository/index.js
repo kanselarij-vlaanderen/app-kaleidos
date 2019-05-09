7e26e23fd832c98a3ed933b027a1b1270ada4a4b
Tom Ploem <tom.ploem@gmail.com>
2019-05-09 10:15:58 +0200

    draft of mailchimp integration

--- mail-chimp-service/repository/index.js
+++ mail-chimp-service/repository/index.js
@@ -3,18 +3,27 @@ import mu from 'mu';
 const getNewsletterInfo = async (agendaId) => {
 
     const query = `
-      PREFIX dct: <http://purl.org/dc/terms/>
+        PREFIX dct: <http://purl.org/dc/terms/>
         PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
         PREFIX besluitvorming: <http://data.vlaanderen.be/ns/besluitvorming#>
         PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
         PREFIX besluit: <http://data.vlaanderen.be/ns/besluit#>
+        PREFIX dbpedia: <http://dbpedia.org/ontology/>
+        PREFIX dct: <http://purl.org/dc/terms/>
         
-        SELECT ?newsletter ?p ?o WHERE {
+        SELECT ?planned_start ?uuid ?title ?text ?subtitle ?publicationDate ?publicationDocDate ?finished WHERE {
             GRAPH <http://mu.semte.ch/application> {
-               ?agenda dct:hasPart ?agendapunt .
-               ?agenda mu:uuid "${agendaId}" .
-               ?agendapunt ext:nieuwsbriefInfo ?newsletter . 
-               ?newsletter ?p ?o
+               <http://data.lblod.info/id/agendas/${agendaId}> besluit:isAangemaaktVoor ?meeting .
+               ?meeting besluit:geplandeStart ?planned_start .
+               <http://data.lblod.info/id/agendas/${agendaId}> dct:hasPart ?agendaitem . 
+               ?newsletter ext:nieuwsbriefInfo ?agendaitem . 
+               OPTIONAL { ?newsletter mu:uuid ?uuid . }
+               OPTIONAL { ?newsletter besluitvorming:inhoud ?text . }
+               OPTIONAL { ?newsletter dbpedia:subtitle ?subtitle . }
+               OPTIONAL { ?newsletter dct:issued ?publicationDate . }
+               OPTIONAL { ?newsletter ext:issuedDocDate ?publicationDocDate . }
+               OPTIONAL { ?newsletter dct:title ?title . }
+               OPTIONAL { ?newsletter ext:afgewerkt ?finished . }
              }
         }`;
     let data = await mu.query(query);
