c4cab5826fb15d07658957cedac20b0debf1ea7a
Tom Ploem <tom.ploem@gmail.com>
2019-05-10 11:13:27 +0200

    dev-dispatcher update

--- config/devdispatcher/dispatcher.ex
+++ config/devdispatcher/dispatcher.ex
@@ -240,6 +240,10 @@ defmodule Dispatcher do
     Proxy.forward conn, path, "http://development-close-meeting-service/"
   end
 
+  match "/send-newsletter/*path" do
+    Proxy.forward conn, path, "http://development-mail-chimp-service/"
+  end
+
   match _ do
     send_resp( conn, 404, "Route not found.  See config/dispatcher.ex" )
   end
