5498b16939b580dd45f4781575344e4ff01ebb92
Karel Kremer <karel.kremer@gmail.com>
2019-07-22 14:15:12 +0200

    compose and config fixes

--- config/resources/domain.lisp
+++ config/resources/domain.lisp
@@ -8,7 +8,6 @@
 (defparameter *cache-count-queries* t)
 (defparameter *supply-cache-headers-p* t
   "when non-nil, cache headers are supplied.  this works together with mu-cache.")
-(setf *cache-model-properties-p* nil)
 (defparameter *include-count-in-paginated-responses* t
   "when non-nil, all paginated listings will contain the number
    of responses in the result object's meta.")
