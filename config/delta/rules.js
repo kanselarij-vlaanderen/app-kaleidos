2f4783045f23c2514160e283476452ac7310ffe5
Aad Versteden <madnificent@gmail.com>
2019-07-30 14:09:53 +0200

    WIP wiring mu-delta to mu-cl-resources
    mu-cl-resources will now receive delta changes from the
    mu-delta-notifier.  This will trigger clearing in the mu-cache also.
    
    Note that if mu-cl-resources receives information about items of which
    it has not received sufficient information before, it will currently
    send broken queries to mu-authorization.  This is a known issue and
    will be tackled in a release nearby.

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -3,14 +3,31 @@ export default [
     match: {
       // form of element is {subject,predicate,object}
       subject: { // type: "uri", value: "ZZZZhttp://www.semanticdesktop.org/ontologies/2007/03/22/nmo#isPartOf" // ZZZZ in this example is just to ensure we don't match anything 8)
-               }
+      }
     },
     callback: {
       url: "http://musearch/update",
-      method: "POST"},
+      method: "POST"
+    },
     options: {
       resourceFormat: "v0.0.0-genesis",
       gracePeriod: 2000,
+      ignoreFromSelf: true
+    }
+  },
+  {
+    match: {
+      // form of element is {subject,predicate,object}
+      subject: { // type: "uri", value: "ZZZZhttp://www.semanticdesktop.org/ontologies/2007/03/22/nmo#isPartOf" // ZZZZ in this example is just to ensure we don't match anything 8)
+      }
+    },
+    callback: {
+      url: "http://resource/update",
+      method: "POST"
+    },
+    options: {
+      resourceFormat: "v0.0.1",
+      gracePeriod: 250,
       ignoreFromSelf: true
     }
   }
