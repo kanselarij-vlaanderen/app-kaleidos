612486e97d7cba72118c2f9b5598f9905f05fa54
Michaël Dierick <michael.dierick@redpencil.io>
2020-02-27 21:22:56 +0100

    Change resources config to be file-bundling-job specific

--- config/resources/job-domain.lisp
+++ config/resources/job-domain.lisp
@@ -1,12 +1,14 @@
-(define-resource job ()
-  :class (s-prefix "cogs:Job")
+(define-resource file-bundling-job ()
+  :class (s-prefix "ext:FileBundlingJob") ; "cogs:Job"
   :properties `((:created       :datetime  ,(s-prefix "dct:created"))
                 (:status        :uri       ,(s-prefix "ext:status"))
                 (:time-started  :datetime  ,(s-prefix "prov:startedAtTime"))
                 (:time-ended    :datetime  ,(s-prefix "prov:endedAtTime"))
                 (:generated     :uri       ,(s-prefix "prov:generated"))
   )
-  ; :resource-base (s-url "http://kanselarij.vo.data.gift/id/jobs/")
+  :has-one `((file              :via     ,(s-prefix "prov:generated")
+                                :as "generated"))
+  ; :resource-base (s-url "http://kanselarij.vo.data.gift/id/file-bundling-jobs/")
   :features '(include-uri)
-  :on-path "jobs"
+  :on-path "file-bundling-jobs"
 )
