5edb866782bdcb8a1a435393fcf31b638e0e2cd8
Michaël Dierick <michael.dierick@redpencil.io>
2020-03-09 11:00:10 +0100

    Bump versions, add custom file-bundling-job creating service

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -67,5 +67,25 @@ export default [
       gracePeriod: 250,
       ignoreFromSelf: false
     }
+  },
+  {
+    match: {
+      predicate: {
+        type: 'uri',
+        value: 'http://mu.semte.ch/vocabularies/ext/status'
+      },
+      object: {
+        type: 'uri'
+      }
+    },
+    callback: {
+      url: 'http://file-bundling-service/delta',
+      method: 'POST'
+    },
+    options: {
+      resourceFormat: 'v0.0.1',
+      gracePeriod: 250,
+      ignoreFromSelf: true
+    }
   }
 ];
