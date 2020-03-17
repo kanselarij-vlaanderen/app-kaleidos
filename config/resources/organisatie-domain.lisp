ab9394d99c30a0f87c9c5bfa645ee4a7f9b7668f
Michaël Dierick <michael.dierick@redpencil.io>
2020-03-17 19:33:47 +0100

    Fix inverse user-organization relation direction

--- config/resources/organisatie-domain.lisp
+++ config/resources/organisatie-domain.lisp
@@ -3,6 +3,7 @@
   :properties `((:name :string ,(s-prefix "skos:prefLabel"))
                 (:identifier :string ,(s-prefix "org:identifier")))
   :has-many `((user :via ,(s-prefix "org:memberOf")
+                             :inverse t
                              :as "members"))
   :resource-base (s-url "https://data.vlaanderen.be/id/organisatie/")
   :features '(include-uri)
