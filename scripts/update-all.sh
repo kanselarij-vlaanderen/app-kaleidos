f5e118be5b3c3506398a8bbd11dc7fa3c27d55c3
Frederik Wouters <wouters_frederik@721548.no-reply.drupal.org>
2020-04-02 11:30:01 +0200

    Add migrations and cache clear to deploy-all script

--- scripts/update-all.sh
+++ scripts/update-all.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-docker-compose  pull && docker-compose  kill && docker-compose  rm -f && docker-compose  up -d
+echo 'GIT PULL';
+echo '--------';
+git pull;
+echo 'UPPING ALL SERVICES';
+echo '--------------------';
+docker-compose  pull && docker-compose  kill && docker-compose  rm -f && docker-compose  up -d;
+echo 'Running Migrations';
+echo '------------------';
+docker-compose restart migrations-service && docker-compose restart cache resource;
