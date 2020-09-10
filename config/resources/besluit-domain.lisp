756a4e65979b24ea6a7d038d58d633c19cf3ecb2
Fred Kroket <woutersf@gmail.com>
2020-09-10 07:22:33 +0200

    KAS-1649: document treatment link

--- config/resources/besluit-domain.lisp
+++ config/resources/besluit-domain.lisp
@@ -119,7 +119,7 @@
              ;(document-container   :via        ,(s-prefix "besluitvorming:genereertVerslag") ; "beslissingsfiche", Property from besluitvorming:Beslissingsactiviteit
              ;                      :as "report")
              (document              :via        ,(s-prefix "besluitvorming:genereertVerslag")
-                                    :as "report")
+                                    :as "report") ;In sommige gevallen waren er hier meerdere voorkomens van. Nader te bekijken hoe wat waarom?
              (newsletter-info       :via        ,(s-prefix "prov:generated")
                                     :as "newsletter-info")
              (decision-result-code  :via        ,(s-prefix "besluitvorming:resultaat")
