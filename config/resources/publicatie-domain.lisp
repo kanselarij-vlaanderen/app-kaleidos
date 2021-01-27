c393b89d37a944f99c03db864c060fc148d6bc63
Fred Kroket <woutersf@gmail.com>
2021-01-27 08:37:14 +0100

    KAS-2160: gevraagde-publicatiedatum

--- config/resources/publicatie-domain.lisp
+++ config/resources/publicatie-domain.lisp
@@ -1,24 +1,25 @@
 (define-resource publication-flow ()
   :class (s-prefix "pub:Publicatieaangelegenheid")
-  :properties `((:publication-number  :string   ,(s-prefix "pub:publicatieNummer"))
-                (:translate-before    :datetime ,(s-prefix "pub:uitersteVertaling")) ;; in de subcase ?
-                (:publish-before      :datetime ,(s-prefix "pub:uiterstePublicatie")) ;; in de subcase ?
-                (:published-at        :datetime ,(s-prefix "pub:publicatieOp")) ;; in de subcase ?                per drukproef/publicatie ??     per drukproef/publicatie ??
-                (:numac-number        :string   ,(s-prefix "pub:numacNummer")) ;; object ? number ?  numac nummer per drukproef/publicatie ??
-                (:remark              :string   ,(s-prefix "pub:publicatieOpmerking")) ;; check of dit nodig is
-                (:priority            :number   ,(s-prefix "pub:prioriteit"))
-                (:created             :datetime ,(s-prefix "dct:created"))
-                (:modified            :datetime ,(s-prefix "dct:modified")))
-  :has-one `((case                    :via      ,(s-prefix "dossier:behandelt")
-                                      :as "case")
-             (publication-status      :via      ,(s-prefix "pub:publicatiestatus")
-                                      :as "status")
-             (publication-type        :via      ,(s-prefix "dct:type")
-                                      :as "type"))
-  :has-many `((subcase                :via      ,(s-prefix "ext:doorloopt") ;; dossier:doorloopt kan niet, mu-cl-resources
-                                      :as "subcases")
-              (contact-person         :via      ,(s-prefix "pub:contactPersoon")
-                                      :as "contact-persons"))
+  :properties `((:publication-number        :string   ,(s-prefix "pub:publicatieNummer"))
+                (:translate-before          :datetime ,(s-prefix "pub:uitersteVertaling")) ;; in de subcase ?
+                (:publish-before            :datetime ,(s-prefix "pub:uiterstePublicatie")) ;; in de subcase ?
+                (:publish-date-requested    :datetime ,(s-prefix "pub:gevraagdePublicatie"))
+                (:published-at              :datetime ,(s-prefix "pub:publicatieOp")) ;; in de subcase ?                per drukproef/publicatie ??     per drukproef/publicatie ??
+                (:numac-number              :string   ,(s-prefix "pub:numacNummer")) ;; object ? number ?  numac nummer per drukproef/publicatie ??
+                (:remark                    :string   ,(s-prefix "pub:publicatieOpmerking")) ;; check of dit nodig is
+                (:priority                  :number   ,(s-prefix "pub:prioriteit"))
+                (:created                   :datetime ,(s-prefix "dct:created"))
+                (:modified                  :datetime ,(s-prefix "dct:modified")))
+  :has-one `((case                          :via      ,(s-prefix "dossier:behandelt")
+                                            :as "case")
+             (publication-status            :via      ,(s-prefix "pub:publicatiestatus")
+                                            :as "status")
+             (publication-type              :via      ,(s-prefix "dct:type")
+                                            :as "type"))
+  :has-many `((subcase                      :via      ,(s-prefix "ext:doorloopt") ;; dossier:doorloopt kan niet, mu-cl-resources
+                                            :as "subcases")
+              (contact-person               :via      ,(s-prefix "pub:contactPersoon")
+                                            :as "contact-persons"))
               ;; mandatees van subcase bij MR,  ongekende mandatee bij niet via MR
   :resource-base (s-url "http://kanselarij.vo.data.gift/id/publicatie-aangelegenheden/")
   :features `(include-uri)
