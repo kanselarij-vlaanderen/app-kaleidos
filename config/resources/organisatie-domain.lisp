b0e35791ca59c72254eb7163f68a80b65139b324
Fred Kroket <woutersf@gmail.com>
2021-02-18 21:51:29 +0100

    KAS-2072: add meta information

--- config/resources/organisatie-domain.lisp
+++ config/resources/organisatie-domain.lisp
@@ -1,8 +1,12 @@
 (define-resource organization ()
   :class (s-prefix "org:Organization")
   :properties `((:name :string ,(s-prefix "skos:prefLabel"))
+                (:created     :datetime       ,(s-prefix "dct:created"))
+                (:modified    :datetime   ,(s-prefix "dct:modified"))
                 ;;(:abbreviation :string ,(s-prefix "ISOcat:abbreviationFor"))
                 (:identifier :string ,(s-prefix "org:identifier")))
+  :has-one `((user                      :via      ,(s-prefix "ext:modifiedBy")
+                                        :as "modified-by"))
   :has-many `((contact-person :via ,(s-prefix "org:memberOf")
                               :inverse t
                               :as "contact-persons"))
