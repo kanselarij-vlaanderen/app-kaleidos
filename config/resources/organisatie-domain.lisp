9b83a92e62549aa93160e6b8d1eff4b49370cf42
Fred Kroket <woutersf@gmail.com>
2021-02-18 15:11:03 +0100

    KAS-2072: add abbreviation and import organisations

--- config/resources/organisatie-domain.lisp
+++ config/resources/organisatie-domain.lisp
@@ -1,6 +1,7 @@
 (define-resource organization ()
   :class (s-prefix "org:Organization")
   :properties `((:name :string ,(s-prefix "skos:prefLabel"))
+                (:abbreviation :string ,(s-prefix "ISOcat:abbreviationFor"))
                 (:identifier :string ,(s-prefix "org:identifier")))
   :has-many `((user :via ,(s-prefix "org:memberOf")
                              :inverse t
