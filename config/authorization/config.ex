6fb8627f9c2cd1582770428040be24824a91645f
Fred Kroket <woutersf@gmail.com>
2021-02-19 09:31:20 +0100

    KAS-2072: only one organization

--- config/authorization/config.ex
+++ config/authorization/config.ex
@@ -83,6 +83,7 @@ defmodule Acl.UserGroups.Config do
       "http://mu.semte.ch/vocabularies/ext/publicatie/Publicatieaangelegenheid",
       "http://mu.semte.ch/vocabularies/ext/publicatie/ContactPersoon",
       "http://mu.semte.ch/vocabularies/ext/publicatie/NumacNummer",
+      "http://www.w3.org/ns/org#Organization",
     ]
   end
 
