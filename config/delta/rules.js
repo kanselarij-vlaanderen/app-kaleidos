9083e16902aec79ac634e444f7425cac48619f97
Michaël Dierick <michael.dierick@redpencil.io>
2021-07-27 16:44:13 +0200

    add staatsblad-related services to docker-compose

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -121,5 +121,43 @@ export default [
       gracePeriod: 5000,
       ignoreFromSelf: true
     }
+  },
+  {
+    match: {
+      predicate: {
+        type: 'uri',
+        value: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'
+      },
+      object: {
+        type: 'uri',
+        value: 'http://data.europa.eu/eli/ontology#LegalResource'
+      }
+    },
+    callback: {
+      url: 'http://uuid-generation/delta',
+      method: 'POST'
+    },
+    options: {
+      resourceFormat: 'v0.0.1',
+      gracePeriod: 250,
+      ignoreFromSelf: false
+    }
+  },
+  {
+    match: {
+      predicate: {
+        type: 'uri',
+        value: 'http://data.europa.eu/eli/ontology#id_local'
+      }
+    },
+    callback: {
+      url: 'http://staatsblad-linking/delta',
+      method: 'POST'
+    },
+    options: {
+      resourceFormat: 'v0.0.1',
+      gracePeriod: 15000,
+      ignoreFromSelf: false
+    }
   }
 ];
