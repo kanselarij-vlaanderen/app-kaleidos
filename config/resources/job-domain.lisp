ee9ceaf8f862e6f74ec44e7518f0b241b81d0b23
Michaël Dierick <michael.dierick@redpencil.io>
2022-03-11 13:54:40 +0100

    add resource, dispatcher & mu-auth config for pub metrics export

--- config/resources/job-domain.lisp
+++ config/resources/job-domain.lisp
@@ -11,3 +11,21 @@
   :features '(include-uri)
   :on-path "file-bundling-jobs"
 )
+
+(define-resource publication-metrics-export-job ()
+  :class (s-prefix "pub:PublicationMetricsExportJob") ; "cogs:Job"
+  :properties `((:created       :datetime  ,(s-prefix "dct:created"))
+                (:status        :url       ,(s-prefix "ext:status"))
+                (:time-started  :datetime  ,(s-prefix "prov:startedAtTime"))
+                (:time-ended    :datetime  ,(s-prefix "prov:endedAtTime"))
+                (:metrics-type  :url       ,(s-prefix "dct:type"))
+                (:config        :string    ,(s-prefix "pub:exportJobConfig")) ; JSON-blob allowing for extendable filter configuration
+  )
+  :has-one `((file              :via     ,(s-prefix "prov:generated")
+                                :as "generated"))
+  :has-one `((user              :via     ,(s-prefix "prov:wasStartedBy")
+                                :as "generated-by"))
+  :resource-base (s-url "http://themis.vlaanderen.be/id/publicatierapport-export-taak/")
+  :features '(include-uri)
+  :on-path "publication-metrics-export-jobs"
+)
