0c3104b8d608d0f07bfe4685d8aa30e7b3ccc24b
Sergio Fenoll <sergio@fenoll.be>
2023-03-28 16:11:22 +0200

    feat: adds health-check endpoint that directly exposes resources service
    All other endpoints go through the cache, which doesn't necessarily fail
    as soon as resource fails. Therefore we add the health check endpoint
    which directly goes through to mu-cl-resources.

--- config/resources/domain.lisp
+++ config/resources/domain.lisp
@@ -37,6 +37,7 @@
 (read-domain-file "email-domain.lisp")
 (read-domain-file "generiek-domain.json")
 (read-domain-file "concept-domain.json")
+(read-domain-file "health-check.lisp")
 
 
 (defcall :get (base-path)
