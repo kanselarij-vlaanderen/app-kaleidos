c9ce705b6eea51dc0ddf9abc7fb34400a683cec3
Sven Valenberghs <sven.valenberghs@faros.be>
2023-05-04 09:51:33 +0200

    comment themis-public graph in mu auth

--- config/authorization/config.ex
+++ config/authorization/config.ex
@@ -294,11 +294,12 @@ defmodule Acl.UserGroups.Config do
             constraint: %ResourceConstraint{
               resource_types: staatsblad_resource_types()
             } }, 
-          %GraphSpec{
-            graph: "http://mu.semte.ch/graphs/themis-public",
-            constraint: %ResourceConstraint{
-              resource_types: themis_export_types()
-            } } ]
+          # %GraphSpec{
+          #   graph: "http://mu.semte.ch/graphs/themis-public",
+          #   constraint: %ResourceConstraint{
+          #     resource_types: themis_export_types()
+          #   } }
+        ]
       },
       %GroupSpec{
         name: "authenticated",
