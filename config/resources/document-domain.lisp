b1b452f79799ed27f5c0bb4303d6981f39912ff9
Sergio Fenoll <sergio@fenoll.be>
2023-12-14 11:33:17 +0100

    fix: add inverse relation for signed piece copy

--- config/resources/document-domain.lisp
+++ config/resources/document-domain.lisp
@@ -38,6 +38,9 @@
                                         :as "signed-piece")
             (piece                      :via      ,(s-prefix "sign:getekendStukKopie")
                                         :as "signed-piece-copy")
+            (piece                      :via      ,(s-prefix "sign:getekendStukKopie")
+                                        :inverse t
+                                        :as "signed-piece-copy-of")
             (subcase                    :via ,(s-prefix "ext:bevatReedsBezorgdeDocumentversie") ;; should be hasMany, not used in frontend yet
                                         :inverse t
                                         :as "linked-subcase")
