bd699ce16a11a10407d687b37e3e7c613bc97990
Sven Valenberghs <sven.valenberghs@faros.be>
2025-11-25 15:55:27 +0100

    KAS-4997 change belga-publication to has-many

--- config/resources/besluit-domain.lisp
+++ config/resources/besluit-domain.lisp
@@ -196,15 +196,15 @@
                                            :as "themis-publication-activities")
               (submission                :via ,(s-prefix "subm:ingediendVoorVergadering")
                                          :inverse t
-                                         :as "submissions"))
+                                         :as "submissions")
+              (belga-publication        :via      ,(s-prefix "ext:heeftBelgaPublicatie")
+                                        :as "belga-publications"))
   :has-one `((agenda                    :via      ,(s-prefix "besluitvorming:behandelt") ;; Final agenda version that is treatened during the meeting
                                         :as "agenda")
               ;; (piece                    :via ,(s-prefix "dossier:genereert") ;; this relation exists in legacy data, but we do not show this in the frontend currently
               ;;                           :as "notes") ;; note: is this a hasOne or hasMany ?
              (mail-campaign             :via      ,(s-prefix "ext:heeftMailCampagnes")
                                         :as "mail-campaign")
-             (belga-publication         :via      ,(s-prefix "ext:heeftBelgaPublicatie")
-                                        :as "belga-publication")
              (concept                   :via      ,(s-prefix "dct:type")
                                         :as "kind")
              (mandatee                  :via        ,(s-prefix "ext:secretarisVoorVergadering")
